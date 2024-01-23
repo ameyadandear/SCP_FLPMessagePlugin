98ee6f8ddc7bebe079fba29c96a6c1b981b73e9c
ameyadandear <157474286+ameyadandear@users.noreply.github.com>
2024-01-23 15:21:06 +0530

    Update Component.js
    additional changes

--- webapp/Component.js
+++ webapp/Component.js
@@ -157,11 +157,7 @@
 			appConfig.popupTitle = "Warning !";
 			appConfig.flpMessage = "This is a test message - SAP could be down....";
 			appConfig.longMessage = 
-				"Lorem ipsum dolor st amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," +
-				" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem " + 
-				"ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore " + 
-				"magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut " + 
-				"labore et dolore magna aliquyam erat";
+				"This is a test message. SAP is under system upgrade and will be down from date: 29 January 2024 till 06 February 2024.";
 			appConfig.closeBtnText = "Close";
 			return appConfig;
 		},
